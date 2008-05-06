0411d3010b5573b31ebb0fbb3f575162622dca65
mpowers <mpowers@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-05-06 03:46:45 +0000

    NX-178: Added the shell for the new scheduled service screen, also added the new privilege for this page
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/beta4@40 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.referenceData.js
+++ nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.referenceData.js
@@ -148,6 +148,11 @@ Sonatype.repoServer.referenceData = {
     repositories : []
       //@todo: there's a discrepancy between routes list and state representation of
       //    the repo data inside routes data
+  },
+  
+  schedule : {
+    id : "",
+    name : "",
   }
 };
 
