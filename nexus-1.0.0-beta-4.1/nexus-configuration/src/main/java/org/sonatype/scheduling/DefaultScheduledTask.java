6ebbb92f79f894091c3d66e97aa9acb1af67ce3c
mpowers <mpowers@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-07-07 19:30:58 +0000

    Copied remotely
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches@605 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus-1.0.0-beta-4.1/nexus-configuration/src/main/java/org/sonatype/scheduling/DefaultScheduledTask.java
@@ -0,0 +1,434 @@
+/*
+ * Nexus: Maven Repository Manager
+ * Copyright (C) 2008 Sonatype Inc.                                                                                                                          
+ * 
+ * This file is part of Nexus.                                                                                                                                  
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
+package org.sonatype.scheduling;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.sonatype.scheduling.iterators.SchedulerIterator;
+import org.sonatype.scheduling.schedules.Schedule;
+
+public class DefaultScheduledTask<T>
+    implements ScheduledTask<T>, Callable<T>
+{
+    private static final AtomicInteger ID_GENERATOR = new AtomicInteger( 0 );
+
+    private final String id;
+
+    private String name;
+
+    private final Class<?> clazz;
+
+    private final DefaultScheduler scheduler;
+
+    private final Callable<T> callable;
+
+    private TaskState taskState;
+
+    private Date scheduledAt;
+
+    private Future<T> future;
+
+    private Throwable throwable;
+
+    private boolean enabled;
+
+    private Date lastRun;
+    
+    private Date nextRun;
+
+    private List<T> results;
+
+    private Schedule schedule;
+
+    private SchedulerIterator scheduleIterator;
+
+    private Map<String, String> taskParams;
+
+    private final boolean storeConfig;
+
+    private boolean manualRun;
+
+    public DefaultScheduledTask( String name, Class<?> clazz, DefaultScheduler scheduler, Callable<T> callable,
+        Schedule schedule, Map<String, String> taskParams, boolean storeConfig )
+    {
+        super();
+
+        this.id = String.valueOf( ID_GENERATOR.getAndIncrement() );
+
+        this.name = name;
+
+        this.clazz = clazz;
+
+        this.scheduler = scheduler;
+
+        this.callable = callable;
+
+        this.taskState = TaskState.SUBMITTED;
+
+        this.enabled = true;
+
+        this.results = new ArrayList<T>();
+
+        this.schedule = schedule;
+
+        this.scheduleIterator = null;
+
+        this.taskParams = taskParams;
+
+        this.storeConfig = storeConfig;
+
+        this.manualRun = false;
+        
+        this.nextRun = null;
+    }
+
+    protected void start()
+    {
+        this.scheduledAt = new Date();
+
+        setFuture( reschedule() );
+    }
+
+    protected Future<T> getFuture()
+    {
+        return future;
+    }
+
+    protected void setFuture( Future<T> future )
+    {
+        this.future = future;
+    }
+
+    protected DefaultScheduler getScheduler()
+    {
+        return scheduler;
+    }
+
+    protected void setTaskState( TaskState state )
+    {
+        if ( !getTaskState().isEndingState() )
+        {
+            this.taskState = state;
+        }
+    }
+
+    protected void setBrokenCause( Throwable e )
+    {
+        this.throwable = e;
+    }
+
+    protected Callable<T> getCallable()
+    {
+        return callable;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public Class<?> getType()
+    {
+        return clazz;
+    }
+
+    public TaskState getTaskState()
+    {
+        return taskState;
+    }
+
+    public Date getScheduledAt()
+    {
+        return scheduledAt;
+    }
+
+    public boolean isStoreConfig()
+    {
+        return storeConfig;
+    }
+
+    public void cancel()
+    {
+        if ( getFuture() != null )
+        {
+            getFuture().cancel( true );
+        }
+
+        setTaskState( TaskState.CANCELLED );
+
+        getScheduler().removeFromTasksMap( this );
+    }
+
+    public void reset()
+    {
+        if ( getFuture() != null )
+        {
+            getFuture().cancel( true );
+        }
+
+        setTaskState( TaskState.SUBMITTED );
+
+        setFuture( reschedule() );
+    }
+
+    public Throwable getBrokenCause()
+    {
+        return throwable;
+    }
+
+    public T get()
+        throws ExecutionException,
+            InterruptedException
+    {
+        return getFuture().get();
+    }
+
+    public T getIfDone()
+    {
+        if ( TaskState.FINISHED.equals( getTaskState() ) )
+        {
+            try
+            {
+                return getFuture().get();
+            }
+            catch ( ExecutionException e )
+            {
+                return null;
+            }
+            catch ( InterruptedException e )
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    protected void setLastRun( Date lastRun )
+    {
+        this.lastRun = lastRun;
+    }
+
+    protected Future<T> reschedule()
+    {
+        SchedulerIterator iter = getScheduleIterator();
+        
+        if ( iter != null &&  !iter.isFinished() )
+        {
+            nextRun = iter.next();
+            
+            long nextTime = nextRun.getTime();
+
+            getScheduler().taskRescheduled( this );
+
+            return getScheduler().getScheduledExecutorService().schedule(
+                this,
+                nextTime - System.currentTimeMillis(),
+                TimeUnit.MILLISECONDS );
+        }
+        else
+        {
+            nextRun = null;
+            
+            return null;
+        }
+    }
+
+    public void runNow()
+    {
+        if ( !manualRun )
+        {
+            manualRun = true;
+            // Just set schedule for now, when done processing will reschedule as normal
+            getScheduler().getScheduledExecutorService().schedule( this, 0, TimeUnit.MILLISECONDS );
+        }
+    }
+
+    public T call()
+        throws Exception
+    {
+        T result = null;
+
+        if ( SchedulerTask.class.isAssignableFrom( getCallable().getClass() ) )
+        {
+            // check for execution
+            while ( !( (SchedulerTask<?>) getCallable() ).allowConcurrentExecution( getScheduler().getActiveTasks() ) )
+            {
+                Thread.sleep( 10000 );
+            };
+        }
+
+        if ( isEnabled() && getTaskState().isActiveOrSubmitted() )
+        {
+            setTaskState( TaskState.RUNNING );
+
+            setLastRun( new Date() );
+
+            try
+            {
+                result = getCallable().call();
+
+                if ( result != null )
+                {
+                    results.add( result );
+                }
+            }
+            catch ( Throwable e )
+            {
+                setBrokenCause( e );
+
+                setTaskState( TaskState.BROKEN );
+
+                // TODO: Should we remove brokeneds?
+                // getScheduler().removeFromTasksMap( this );
+
+                if ( Exception.class.isAssignableFrom( e.getClass() ) )
+                {
+                    // this is an exception, pass it further
+                    throw (Exception) e;
+                }
+                else
+                {
+                    // this is a Throwable or Error instance, pack it into an exception and rethrow
+                    throw new TaskExecutionException( e );
+                }
+            }
+        }
+
+        Future<T> nextFuture = null;
+
+        // If manually running, just grab the previous future and use that
+        if ( manualRun )
+        {
+            nextFuture = getFuture();
+            manualRun = false;
+        }
+        // Otherwise, grab the next one
+        else
+        {
+            nextFuture = reschedule();
+        }
+
+        if ( nextFuture != null )
+        {
+            setTaskState( TaskState.WAITING );
+
+            setFuture( nextFuture );
+        }
+        else
+        {
+            if ( getSchedule() != null )
+            {
+                setTaskState( TaskState.FINISHED );
+    
+                getScheduler().removeFromTasksMap( this );
+            }
+            else
+            {
+                // Manual schedule types just stay submitted when not running
+                setTaskState( TaskState.SUBMITTED );
+            }
+        }
+
+        return result;
+    }
+
+    // IteratingTask
+
+    public Date getLastRun()
+    {
+        return lastRun;
+    }
+
+    public Date getNextRun()
+    {
+        return nextRun;
+    }
+
+    public boolean isEnabled()
+    {
+        return enabled;
+    }
+
+    public void setEnabled( boolean enabled )
+    {
+        this.enabled = enabled;
+    }
+
+    public List<T> getResults()
+    {
+        return results;
+    }
+
+    // ScheduledTask
+
+    public Schedule getSchedule()
+    {
+        return schedule;
+    }
+
+    public void setSchedule( Schedule schedule )
+    {
+        this.schedule = schedule;
+
+        this.scheduleIterator = null;
+    }
+
+    public SchedulerIterator getScheduleIterator()
+    {
+        if ( scheduleIterator == null && getSchedule() != null )
+        {
+            scheduleIterator = getSchedule().getIterator();
+        }
+        
+        return scheduleIterator;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Map<String, String> getTaskParams()
+    {
+        if ( taskParams == null )
+        {
+            taskParams = new HashMap<String, String>();
+        }
+        return taskParams;
+    }
+}
