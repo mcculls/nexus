017975960fe2834506d3d2003fa1e20d6f7756f5
tstevens <tstevens@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-07-17 13:56:07 +0000

    Restructured test-harness, stay tuned for more changes
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches@727 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ new-test-harness/src/test/java/org/sonatype/nexus/integrationtests/nexus166/Nexus166SampleTest.java
@@ -0,0 +1,16 @@
+package org.sonatype.nexus.integrationtests.nexus166;
+
+import org.junit.Test;
+import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
+
+
+public class Nexus166SampleTest extends AbstractNexusIntegrationTest
+{
+
+    @Test
+    public void doTest()
+    {
+        System.out.println( "This is just an example test" );
+    }
+    
+}
