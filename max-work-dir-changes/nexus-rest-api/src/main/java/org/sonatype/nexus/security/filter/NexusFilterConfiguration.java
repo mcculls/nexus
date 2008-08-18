bb290313f8ede6a6241735cf1a92876c1cdce404
mpowers <mpowers@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-08-18 15:35:00 +0000

    Removed file/folder
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches@1249 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- max-work-dir-changes/nexus-rest-api/src/main/java/org/sonatype/nexus/security/filter/NexusFilterConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.sonatype.nexus.security.filter;
-
-import org.sonatype.plexus.jsecurity.web.filter.PlexusConfiguration;
-
-public class NexusFilterConfiguration
-    extends PlexusConfiguration
-{
-
-}
