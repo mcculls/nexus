2eddc632432b8ba08e730f681b60444d4fcf03a5
velo <velo@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-04-24 15:57:02 +0000

    IN PROGRESS - issue NEXUS-1923: Create ITs to validate incremental index consumption https://issues.sonatype.org/browse/NEXUS-1923
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/damian-indexer-1.3.x@3975 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1923/AbstractNexus1923.java
+++ nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1923/AbstractNexus1923.java
@@ -2,6 +2,7 @@ package org.sonatype.nexus.integrationtests.nexus1923;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,11 +16,14 @@ import org.restlet.data.Status;
 import org.sonatype.nexus.index.context.IndexingContext;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
 import org.sonatype.nexus.rest.model.NexusArtifact;
+import org.sonatype.nexus.rest.model.RepositoryGroupMemberRepository;
+import org.sonatype.nexus.rest.model.RepositoryGroupResource;
 import org.sonatype.nexus.rest.model.RepositoryResource;
 import org.sonatype.nexus.rest.model.RepositoryResourceRemoteStorage;
 import org.sonatype.nexus.rest.model.ScheduledServiceBaseResource;
 import org.sonatype.nexus.rest.model.ScheduledServicePropertyResource;
 import org.sonatype.nexus.tasks.descriptors.ReindexTaskDescriptor;
+import org.sonatype.nexus.test.utils.GroupMessageUtil;
 import org.sonatype.nexus.test.utils.RepositoryMessageUtil;
 import org.sonatype.nexus.test.utils.SearchMessageUtil;
 import org.sonatype.nexus.test.utils.TaskScheduleUtil;
@@ -28,33 +32,46 @@ public abstract class AbstractNexus1923
     extends AbstractNexusIntegrationTest
 {
     protected RepositoryMessageUtil repoUtils;
+
+    protected GroupMessageUtil groupUtils;
+
     protected SearchMessageUtil searchUtils;
-        
+
     protected static final String HOSTED_REPO_ID = "incremental_repo";
+
     protected static final String OTHER_HOSTED_REPO_ID = "incremental_repo_other";
+
+    protected static final String THIRD_HOSTED_REPO_ID = "incremental_repo_third";
+
     protected static final String PROXY_REPO_ID = "incremental_repo_proxy";
+
     protected static final String FIRST_ARTIFACT = "firstArtifact";
+
     protected static final String SECOND_ARTIFACT = "secondArtifact";
+
     protected static final String THIRD_ARTIFACT = "thirdArtifact";
+
     protected static final String FOURTH_ARTIFACT = "fourthArtifact";
+
     protected static final String FIFTH_ARTIFACT = "fifthArtifact";
+
     protected static final String HOSTED_REINDEX_TASK_NAME = "incremental_reindex";
+
     protected static final String OTHER_HOSTED_REINDEX_TASK_NAME = "incremental_reindex_other";
-    protected static final String PROXY_REINDEX_TASK_NAME = "incremental_reindex_proxy";    
-    
+
+    protected static final String PROXY_REINDEX_TASK_NAME = "incremental_reindex_proxy";
+
     public AbstractNexus1923()
         throws Exception
     {
         super();
-        
+
         this.repoUtils =
-            new RepositoryMessageUtil( 
-                this.getJsonXStream(), 
-                MediaType.APPLICATION_JSON,
-                getRepositoryTypeRegistry() );
-        
+            new RepositoryMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON, getRepositoryTypeRegistry() );
+        this.groupUtils = new GroupMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
+
         this.searchUtils = new SearchMessageUtil();
-        
+
         FileUtils.deleteDirectory( getHostedRepositoryStorageDirectory() );
         FileUtils.deleteDirectory( getHostedRepositoryLocalIndexDirectory() );
         FileUtils.deleteDirectory( getHostedRepositoryRemoteIndexDirectory() );
@@ -65,21 +82,21 @@ public abstract class AbstractNexus1923
         FileUtils.deleteDirectory( getOtherHostedRepositoryLocalIndexDirectory() );
         FileUtils.deleteDirectory( getOtherHostedRepositoryRemoteIndexDirectory() );
     }
-    
+
     private RepositoryResource createRepository()
     {
         RepositoryResource resource = new RepositoryResource();
-        
+
         resource.setProvider( "maven2" );
         resource.setFormat( "maven2" );
         resource.setRepoPolicy( "release" );
         resource.setChecksumPolicy( "ignore" );
         resource.setBrowseable( true );
         resource.setIndexable( true );
-        
+
         return resource;
     }
-    
+
     protected void createHostedRepository()
         throws Exception
     {
@@ -88,9 +105,9 @@ public abstract class AbstractNexus1923
         resource.setName( HOSTED_REPO_ID );
         resource.setRepoType( "hosted" );
         resource.setAllowWrite( true );
-        repoUtils.createRepository( resource );   
+        repoUtils.createRepository( resource );
     }
-    
+
     protected void createProxyRepository()
         throws Exception
     {
@@ -100,12 +117,12 @@ public abstract class AbstractNexus1923
         resource.setRepoType( "proxy" );
         resource.setAllowWrite( false );
         resource.setDownloadRemoteIndexes( true );
-        RepositoryResourceRemoteStorage remoteStorage = new RepositoryResourceRemoteStorage();        
+        RepositoryResourceRemoteStorage remoteStorage = new RepositoryResourceRemoteStorage();
         remoteStorage.setRemoteStorageUrl( getBaseNexusUrl() + "content/repositories/" + HOSTED_REPO_ID );
         resource.setRemoteStorage( remoteStorage );
         repoUtils.createRepository( resource );
     }
-    
+
     protected void createOtherHostedRepository()
         throws Exception
     {
@@ -114,16 +131,44 @@ public abstract class AbstractNexus1923
         resource.setName( OTHER_HOSTED_REPO_ID );
         resource.setRepoType( "hosted" );
         resource.setAllowWrite( true );
-        repoUtils.createRepository( resource );   
+        repoUtils.createRepository( resource );
     }
-    
-    private String createReindexTask( String repositoryId, String taskName )
+
+    protected void createThirdHostedRepository()
+        throws Exception
+    {
+        RepositoryResource resource = createRepository();
+        resource.setId( THIRD_HOSTED_REPO_ID );
+        resource.setName( THIRD_HOSTED_REPO_ID );
+        resource.setRepoType( "hosted" );
+        resource.setAllowWrite( true );
+        repoUtils.createRepository( resource );
+    }
+
+    protected void createGroup( String groupId, String... repoIds )
+        throws IOException
+    {
+        RepositoryGroupResource group = new RepositoryGroupResource();
+        group.setId( groupId );
+        group.setName( groupId );
+        group.setFormat( "maven2" );
+
+        for ( String repoId : repoIds )
+        {
+            RepositoryGroupMemberRepository repo = new RepositoryGroupMemberRepository();
+            repo.setId( repoId );
+            group.addRepository( repo );
+        }
+        groupUtils.createGroup( group );
+    }
+
+    protected String createReindexTask( String repositoryId, String taskName )
         throws Exception
     {
         ScheduledServicePropertyResource prop = new ScheduledServicePropertyResource();
         prop.setId( "repositoryOrGroupId" );
         prop.setValue( "repo_" + repositoryId );
-    
+
         ScheduledServiceBaseResource scheduledTask = new ScheduledServiceBaseResource();
         scheduledTask.setEnabled( true );
         scheduledTask.setId( null );
@@ -132,145 +177,145 @@ public abstract class AbstractNexus1923
         scheduledTask.setSchedule( "manual" );
         scheduledTask.addProperty( prop );
         Status status = TaskScheduleUtil.create( scheduledTask );
-        
+
         Assert.assertTrue( status.isSuccess() );
-        
+
         return TaskScheduleUtil.getTask( taskName ).getId();
     }
-    
+
     protected String createHostedReindexTask()
         throws Exception
     {
         return createReindexTask( HOSTED_REPO_ID, HOSTED_REINDEX_TASK_NAME );
     }
-    
+
     protected String createProxyReindexTask()
         throws Exception
     {
         return createReindexTask( PROXY_REPO_ID, PROXY_REINDEX_TASK_NAME );
     }
-    
+
     protected String createOtherHostedReindexTask()
         throws Exception
     {
         return createReindexTask( OTHER_HOSTED_REPO_ID, OTHER_HOSTED_REINDEX_TASK_NAME );
     }
-    
-    private void reindexRepository( String taskId, String taskName )
+
+    protected void reindexRepository( String taskId, String taskName )
         throws Exception
     {
         TaskScheduleUtil.run( taskId );
-        
+
         TaskScheduleUtil.waitForTask( taskName, 300 );
     }
-    
+
     protected void reindexHostedRepository( String taskId )
         throws Exception
     {
         reindexRepository( taskId, HOSTED_REINDEX_TASK_NAME );
     }
-    
+
     protected void reindexProxyRepository( String taskId )
         throws Exception
     {
         reindexRepository( taskId, PROXY_REINDEX_TASK_NAME );
     }
-    
+
     protected void reindexOtherHostedRepository( String taskId )
         throws Exception
     {
         reindexRepository( taskId, OTHER_HOSTED_REINDEX_TASK_NAME );
     }
-    
+
     private File getRepositoryLocalIndexDirectory( String repositoryId )
     {
         return new File( AbstractNexusIntegrationTest.nexusWorkDir + "/indexer/" + repositoryId + "-local/" );
     }
-    
+
     protected File getHostedRepositoryLocalIndexDirectory()
     {
         return getRepositoryLocalIndexDirectory( HOSTED_REPO_ID );
     }
-    
+
     protected File getProxyRepositoryLocalIndexDirectory()
     {
         return getRepositoryLocalIndexDirectory( PROXY_REPO_ID );
     }
-    
+
     protected File getOtherHostedRepositoryLocalIndexDirectory()
     {
         return getRepositoryLocalIndexDirectory( OTHER_HOSTED_REPO_ID );
     }
-    
+
     private File getRepositoryRemoteIndexDirectory( String repositoryId )
     {
         return new File( AbstractNexusIntegrationTest.nexusWorkDir + "/indexer/" + repositoryId + "-remote/" );
     }
-    
+
     protected File getHostedRepositoryRemoteIndexDirectory()
     {
         return getRepositoryRemoteIndexDirectory( HOSTED_REPO_ID );
     }
-    
+
     protected File getProxyRepositoryRemoteIndexDirectory()
     {
         return getRepositoryRemoteIndexDirectory( PROXY_REPO_ID );
     }
-    
+
     protected File getOtherHostedRepositoryRemoteIndexDirectory()
     {
         return getRepositoryRemoteIndexDirectory( OTHER_HOSTED_REPO_ID );
     }
-    
-    private File getRepositoryStorageDirectory( String repositoryId )
+
+    protected File getRepositoryStorageDirectory( String repositoryId )
     {
         return new File( AbstractNexusIntegrationTest.nexusWorkDir + "/storage/" + repositoryId + "/" );
     }
-    
+
     protected File getHostedRepositoryStorageDirectory()
     {
         return getRepositoryStorageDirectory( HOSTED_REPO_ID );
     }
-    
+
     protected File getProxyRepositoryStorageDirectory()
     {
         return getRepositoryStorageDirectory( PROXY_REPO_ID );
     }
-    
+
     protected File getOtherHostedRepositoryStorageDirectory()
     {
         return getRepositoryStorageDirectory( OTHER_HOSTED_REPO_ID );
     }
-    
-    private File getRepositoryIndex( File directory )
+
+    protected File getRepositoryIndex( File directory )
     {
-        return new File( directory, IndexingContext.INDEX_FILE + ".gz");
+        return new File( directory, IndexingContext.INDEX_FILE + ".gz" );
     }
-    
+
     protected File getHostedRepositoryIndex()
     {
         return getRepositoryIndex( getHostedRepositoryStorageIndexDirectory() );
     }
-    
+
     protected File getProxyRepositoryIndex()
     {
         return getRepositoryIndex( getProxyRepositoryStorageIndexDirectory() );
     }
-    
+
     protected File getOtherHostedRepositoryIndex()
     {
         return getRepositoryIndex( getOtherHostedRepositoryStorageIndexDirectory() );
     }
-    
-    private Properties getRepositoryIndexProperties( File baseDir )
+
+    protected Properties getRepositoryIndexProperties( File baseDir )
         throws Exception
     {
         Properties props = new Properties();
-        
+
         FileInputStream fis = null;
         try
         {
-            fis = new FileInputStream( new File( baseDir , IndexingContext.INDEX_FILE + ".properties" ) );
+            fis = new FileInputStream( new File( baseDir, IndexingContext.INDEX_FILE + ".properties" ) );
             props.load( fis );
         }
         finally
@@ -280,69 +325,69 @@ public abstract class AbstractNexus1923
                 fis.close();
             }
         }
-        
+
         return props;
     }
-    
+
     protected Properties getHostedRepositoryIndexProperties()
         throws Exception
     {
         return getRepositoryIndexProperties( getHostedRepositoryStorageIndexDirectory() );
     }
-    
+
     protected Properties getProxyRepositoryIndexProperties()
         throws Exception
     {
         return getRepositoryIndexProperties( getProxyRepositoryStorageIndexDirectory() );
     }
-    
+
     protected Properties getOtherHostedRepositoryIndexProperties()
         throws Exception
     {
         return getRepositoryIndexProperties( getOtherHostedRepositoryStorageIndexDirectory() );
     }
-    
-    private File getRepositoryIndexIncrement( File directory, String id )
+
+    protected File getRepositoryIndexIncrement( File directory, String id )
     {
         return new File( directory, IndexingContext.INDEX_FILE + "." + id + ".gz" );
     }
-    
+
     protected File getHostedRepositoryIndexIncrement( String id )
     {
-        return getRepositoryIndexIncrement( getHostedRepositoryStorageIndexDirectory(), id ); 
+        return getRepositoryIndexIncrement( getHostedRepositoryStorageIndexDirectory(), id );
     }
-    
+
     protected File getProxyRepositoryIndexIncrement( String id )
     {
-        return getRepositoryIndexIncrement( getProxyRepositoryStorageIndexDirectory(), id ); 
+        return getRepositoryIndexIncrement( getProxyRepositoryStorageIndexDirectory(), id );
     }
-    
+
     protected File getOtherHostedRepositoryIndexIncrement( String id )
     {
-        return getRepositoryIndexIncrement( getOtherHostedRepositoryStorageIndexDirectory(), id ); 
+        return getRepositoryIndexIncrement( getOtherHostedRepositoryStorageIndexDirectory(), id );
     }
-    
-    private File getRepositoryStorageIndexDirectory( String repositoryId )
+
+    protected File getRepositoryStorageIndexDirectory( String repositoryId )
     {
         return new File( AbstractNexusIntegrationTest.nexusWorkDir + "/storage/" + repositoryId + "/.index/" );
     }
-    
+
     protected File getHostedRepositoryStorageIndexDirectory()
     {
         return getRepositoryStorageIndexDirectory( HOSTED_REPO_ID );
     }
-    
+
     protected File getProxyRepositoryStorageIndexDirectory()
     {
         return getRepositoryStorageIndexDirectory( PROXY_REPO_ID );
     }
-    
+
     protected File getOtherHostedRepositoryStorageIndexDirectory()
     {
         return getRepositoryStorageIndexDirectory( OTHER_HOSTED_REPO_ID );
     }
-    
-    private void validateCurrentIncrementalCounter( Properties properties, Integer current )
+
+    protected void validateCurrentIncrementalCounter( Properties properties, Integer current )
         throws Exception
     {
         if ( current == null )
@@ -351,90 +396,89 @@ public abstract class AbstractNexus1923
         }
         else
         {
-            Assert.assertEquals( properties.getProperty( IndexingContext.INDEX_CHUNK_COUNTER ), Integer.toString( current ) );
+            Assert.assertEquals( properties.getProperty( IndexingContext.INDEX_CHUNK_COUNTER ),
+                                 Integer.toString( current ) );
         }
     }
-    
+
     protected void validateCurrentHostedIncrementalCounter( Integer current )
         throws Exception
     {
         validateCurrentIncrementalCounter( getHostedRepositoryIndexProperties(), current );
     }
-    
+
     protected void validateCurrentProxyIncrementalCounter( Integer current )
         throws Exception
     {
         validateCurrentIncrementalCounter( getProxyRepositoryIndexProperties(), current );
     }
-    
+
     protected void validateCurrentOtherHostedIncrementalCounter( int current )
         throws Exception
     {
         validateCurrentIncrementalCounter( getOtherHostedRepositoryIndexProperties(), current );
     }
-    
+
     private void searchForArtifactInIndex( String artifact, String repositoryId, boolean shouldFind )
         throws Exception
     {
         Map<String, String> args = new HashMap<String, String>();
         args.put( "r", repositoryId );
-        
+
         if ( FIRST_ARTIFACT.equals( artifact ) )
-        {            
+        {
             args.put( "a", "ant" );
         }
         else if ( SECOND_ARTIFACT.equals( artifact ) )
-        {            
+        {
             args.put( "a", "asm" );
         }
         else if ( THIRD_ARTIFACT.equals( artifact ) )
-        {            
+        {
             args.put( "a", "commons-attributes-api" );
         }
         else if ( FOURTH_ARTIFACT.equals( artifact ) )
-        {            
+        {
             args.put( "a", "commons-cli" );
         }
         else if ( FIFTH_ARTIFACT.equals( artifact ) )
-        {            
+        {
             args.put( "a", "commons-io" );
         }
-        
+
         List<NexusArtifact> artifacts = searchUtils.searchFor( args );
-        
+
         Assert.assertEquals( artifacts.size() > 0, shouldFind );
     }
-    
+
     protected void searchForArtifactInHostedIndex( String artifact, boolean shouldFind )
         throws Exception
     {
         searchForArtifactInIndex( artifact, HOSTED_REPO_ID, shouldFind );
     }
-    
+
     protected void searchForArtifactInProxyIndex( String artifact, boolean shouldFind )
         throws Exception
     {
         searchForArtifactInIndex( artifact, PROXY_REPO_ID, shouldFind );
     }
-    
+
     protected void searchForArtifactInOtherHostedIndex( String artifact, boolean shouldFind )
         throws Exception
     {
         searchForArtifactInIndex( artifact, OTHER_HOSTED_REPO_ID, shouldFind );
     }
-    
+
     protected void deleteAllNonHiddenContent( File directory )
         throws Exception
     {
-        if ( directory.isDirectory()
-            && directory.exists() )
+        if ( directory.isDirectory() && directory.exists() )
         {
             File[] files = directory.listFiles();
-            
-            if ( files != null
-                && files.length > 0 )
+
+            if ( files != null && files.length > 0 )
             {
-                for ( int i = 0 ; i < files.length ; i++ )
+                for ( int i = 0; i < files.length; i++ )
                 {
                     if ( files[i].isDirectory() )
                     {
--- nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1923/Nexus1923GroupIncrementalIndex.java
+++ nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1923/Nexus1923GroupIncrementalIndex.java
@@ -1,21 +1,89 @@
 package org.sonatype.nexus.integrationtests.nexus1923;
 
+import java.io.File;
+import java.util.Properties;
+
+import junit.framework.Assert;
+
+import org.codehaus.plexus.util.FileUtils;
 import org.junit.Test;
 
 public class Nexus1923GroupIncrementalIndex
-extends AbstractNexus1923
-{    
+    extends AbstractNexus1923
+{
+    private static final String INDEX_GROUP = "index_group";
+
+    private static final String INDEX_GROUP_TASK = INDEX_GROUP + "-task";
+
     public Nexus1923GroupIncrementalIndex()
         throws Exception
     {
         super();
     }
-    
+
     @Test
     public void validateIncrementalIndexesCreated()
         throws Exception
-    { 
+    {
         createHostedRepository();
         createOtherHostedRepository();
+        createThirdHostedRepository();
+
+        createGroup( INDEX_GROUP, HOSTED_REPO_ID, OTHER_HOSTED_REPO_ID, THIRD_HOSTED_REPO_ID );
+
+        String reindexId = createReindexTask( INDEX_GROUP, INDEX_GROUP_TASK );
+
+        FileUtils.copyDirectoryStructure( getTestFile( FIRST_ARTIFACT ), getHostedRepositoryStorageDirectory() );
+
+        reindexRepository( reindexId, INDEX_GROUP_TASK );
+
+        Assert.assertTrue( getHostedRepositoryIndex().exists() );
+        Assert.assertFalse( getHostedRepositoryIndexIncrement( "1" ).exists() );
+        validateCurrentHostedIncrementalCounter( null );
+
+        Assert.assertTrue( getGroupIndex().exists() );
+        Assert.assertFalse( getGroupIndexIncrement( "1" ).exists() );
+        validateCurrentGroupIncrementalCounter( null );
+
+        FileUtils.copyDirectoryStructure( getTestFile( SECOND_ARTIFACT ), getOtherHostedRepositoryStorageDirectory() );
+
+        reindexRepository( reindexId, INDEX_GROUP_TASK );
+
+        Assert.assertTrue( getOtherHostedRepositoryIndex().exists() );
+        Assert.assertFalse( getOtherHostedRepositoryIndexIncrement( "1" ).exists() );
+        validateCurrentHostedIncrementalCounter( null );
+
+        Assert.assertTrue( getGroupIndex().exists() );
+        Assert.assertFalse( getGroupIndexIncrement( "1" ).exists() );
+        Assert.assertFalse( getGroupIndexIncrement( "2" ).exists() );
+        validateCurrentGroupIncrementalCounter( null );
+
+    }
+
+    protected void validateCurrentGroupIncrementalCounter( Integer current )
+        throws Exception
+    {
+        validateCurrentIncrementalCounter( getGroupIndexProperties(), current );
+    }
+
+    private Properties getGroupIndexProperties()
+        throws Exception
+    {
+        return getRepositoryIndexProperties( getGroupStorageIndexDirectory() );
+    }
+
+    private File getGroupIndexIncrement( String id )
+    {
+        return getRepositoryIndexIncrement( getGroupStorageIndexDirectory(), id );
+    }
+
+    private File getGroupIndex()
+    {
+        return getRepositoryIndex( getGroupStorageIndexDirectory() );
+    }
+
+    protected File getGroupStorageIndexDirectory()
+    {
+        return getRepositoryStorageIndexDirectory( INDEX_GROUP );
     }
 }
