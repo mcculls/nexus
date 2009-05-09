886e87515ea6d5b3b5c6699d3d510512737db2b7
plightbo <plightbo@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-05-09 17:50:10 +0000

    only open the connection once... also clean it up properly afterwards
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/nexus-1815@4138 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-mock/src/test/java/org/sonatype/nexus/mock/SeleniumTest.java
+++ nexus-mock/src/test/java/org/sonatype/nexus/mock/SeleniumTest.java
@@ -58,25 +58,35 @@ public abstract class SeleniumTest extends NexusTestCase {
     public static void openTunnel() throws Exception {
         NexusTestCase.startNexus();
 
-        int port = PropUtil.get("jettyPort", 12345);
 
-        // spin up SSH connection
-        sshConn = new Connection("grid.sonatype.org", 10023);
-        sshConn.connect();
+        if (sshConn == null) {
+            int port = PropUtil.get("jettyPort", 12345);
 
-        // authenticate
-        File pemFile = new File(System.getenv().get("HOME") + "/.ssh/nexus_selenium_rsa");
-        String password = null;
-        if (!pemFile.exists()) {
-            pemFile = new File(System.getenv().get("HOME") + "/.ssh/id_rsa");
-            password = System.getProperty("sshPassword");
-        }
+            // spin up SSH connection
+            sshConn = new Connection("grid.sonatype.org", 10023);
+            sshConn.connect();
 
-        boolean isAuthenticated = sshConn.authenticateWithPublicKey("hudson", pemFile, password);
-        if (isAuthenticated) {
-            System.out.println("Requesting remote port forwarding for port " + port);
-            sshConn.requestRemotePortForwarding("", port, "localhost", port);
-            sshConn.createLocalPortForwarder(4444, "localhost", 4444);
+            // authenticate
+            File pemFile = new File(System.getenv().get("HOME") + "/.ssh/nexus_selenium_rsa");
+            String password = null;
+            if (!pemFile.exists()) {
+                pemFile = new File(System.getenv().get("HOME") + "/.ssh/id_rsa");
+                password = System.getProperty("sshPassword");
+            }
+
+            boolean isAuthenticated = sshConn.authenticateWithPublicKey("hudson", pemFile, password);
+            if (isAuthenticated) {
+                System.out.println("Requesting remote port forwarding for port " + port);
+                sshConn.requestRemotePortForwarding("", port, "localhost", port);
+                sshConn.createLocalPortForwarder(4444, "localhost", 4444);
+            }
+
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    sshConn.close();
+                }
+            }));
         }
     }
 
